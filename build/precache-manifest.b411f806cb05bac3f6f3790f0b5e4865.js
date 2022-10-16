3cec773d6fab0e1b43115b22c8104c239cbd93b4
AmitSingh80 <heyamitsingh7@gmail.com>
2022-10-16 17:05:37 +0530

    deploying app

---
+++ build/precache-manifest.b411f806cb05bac3f6f3790f0b5e4865.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87c01d75d5fa87eb0b1348801683cd5",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "ad4a03cc08101465f7c4",
+    "url": "/covid-19-tracker/static/css/2.bd459b17.chunk.css"
+  },
+  {
+    "revision": "9267134ed89027e1af73",
+    "url": "/covid-19-tracker/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "ad4a03cc08101465f7c4",
+    "url": "/covid-19-tracker/static/js/2.fe815301.chunk.js"
+  },
+  {
+    "revision": "e00ca5403d49fbbe83f0e777d485a623",
+    "url": "/covid-19-tracker/static/js/2.fe815301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9267134ed89027e1af73",
+    "url": "/covid-19-tracker/static/js/main.63bc9ff2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/covid-19-tracker/static/js/main.63bc9ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081748b171dd253e3c97",
+    "url": "/covid-19-tracker/static/js/runtime-main.d3164cfc.js"
+  }
+]);
